9b17c526937af0d9cfc50e952951be2fee138cf3
Sadanand09 <sadanandgurude9@gmail.com>
2026-02-17 08:53:10 +0530

    Implement SplashScreen

---
+++ app/splash-screen.tsx
@@ -0,0 +1,42 @@
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SplashScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-[#EA7A53]">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View>
+          <Image
+            source={require("@/assets/images/splash-pattern.png")}
+            resizeMode="contain"
+            className="mt-10 w-full"
+          />
+        </View>
+        <View className="px-6 p-12">
+          <View className='items-center'> 
+            <Text className="text-white text-4xl font-bold mb-2">
+              Gain Financial Clarity
+            </Text>
+
+            <Text className="text-white/90 text-base mb-8">
+              Track, analyze and cancel with ease
+            </Text>
+          </View>
+
+          {/* CTA Button */}
+          <TouchableOpacity
+            activeOpacity={0.8}
+            className="bg-white rounded-full py-4 items-center"
+          >
+            <Text className="text-[#1F2937] text-base font-semibold">
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default SplashScreen;
